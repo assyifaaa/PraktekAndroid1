728f1df6a7905ccb082f6d430f497ebfa0c5e50c
assyifaaa <assyifaa.hasna@gmail.com>
2020-05-07 05:17:01 +0700

    Penambahan HitungActivity, panggil dengan internet

--- app/src/main/java/com/praktekandroid/MainActivity.kt
+++ app/src/main/java/com/praktekandroid/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.praktekandroid
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,10 +17,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val TextView1 = findViewById<TextView>(R.id.TextView1)
+        val button1 = findViewById<Button>(R.id.button1)
 
         TextView1.setOnClickListener {
             Toast.makeText(this, "hai", Toast.LENGTH_LONG).show()
         }
+
+        button1.setOnClickListener{
+            val intent = Intent (this@MainActivity, HitungActivity ::class.java)
+            startActivity(intent)
+        }
+
     }
 }
 
